0fe75a44e8d3aaa92e7b6730a71d25f3b6179ffa
fohofmann <felix.o.hofmann@gmail.com>
2024-09-17 08:27:05 +0200

    github download, integration tissue models

--- BodyComposition/bin/pre_download_models.py
+++ BodyComposition/bin/pre_download_models.py
@@ -7,14 +7,26 @@ from pathlib import Path
 import os
 from time import time
 from BodyComposition.utils.config import update_config
+import requests
+from tqdm import tqdm
+import zipfile
+import shutil
+
+# set up logging
+path_log = Path(f'./logs/pre_downloadmodels_{int(time())}.log')
+path_log.parent.mkdir(parents=True, exist_ok=True)
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=path_log)
 
 # defining models per collection
 definition_pipelines = {
-    'BodyComposition': ['VertebralBodiesCT-ResEncL', 'TotalSegmentator-body', 'TotalSegmentator-tissue_types'],
-    'BodyCompositionFast': ['VertebralBodiesCT-ResEncM', 'TotalSegmentator-tissue_types' ],
+    'BodyComposition': ['VertebralBodiesCT-ResEncL', 'BodyCompositionCT-ResEncL'],
+    'BodyCompositionFast': ['VertebralBodiesCT-ResEncM', 'BodyCompositionCT-ResEncM'],
     'SarcopeniaTotalSegmentator': ['TotalSegmentator-spine', 'TotalSegmentator-muscles', 'TotalSegmentator-body', 'TotalSegmentator-vertebrae_body', 'TotalSegmentator-tissue_types'],
     'SarcopeniaTotalSegmentatorFast': ['TotalSegmentator-spine', 'TotalSegmentator-tissue_types'],
-    'SarcopeniaStanford': ['Stanford-Spine', 'TotalSegmentator-body', 'TotalSegmentator-vertebrae_body', 'TotalSegmentator-tissue_types'],
+    'SarcopeniaStanfordFast': ['Stanford-Spine', 'Stanford-Tissue'],
+    'BodyAndOrganAnalysis': ['BodyAndOrganAnalysis', 'TotalSegmentator-spine'],
 }
 
 # defining sources
@@ -29,14 +41,15 @@ definition_sources = {
         'hf_id': 'fhofmann/VertebralBodiesCT-ResEncM',
         'local_id': 'int-vertebrae'
     },
-    # 'BodyCompositionCT-ResEncL': {
-    #     'source': 'huggingface',
-    #     'hf_id': 'fhofmann/BodyCompositionCT-ResEncL'
-    # }, # soon at your service
-    # 'BodyCompositionCT-ResEncM': {
-    #     'source': 'huggingface',
-    #     'hf_id': 'fhofmann/BodyCompositionCT-ResEncM'
-    # }, # soon at your service
+    'BodyCompositionCT-ResEncL': {
+         'source': 'huggingface',
+         'hf_id': 'fhofmann/BodyCompositionCT-ResEncL',
+         'local_id': 'int-bodycomposition'
+    },
+    'BodyCompositionCT-ResEncM': {
+         'source': 'huggingface',
+         'hf_id': 'fhofmann/BodyCompositionCT-ResEncM'
+    },
     'TotalSegmentator-spine': {
         'source': 'totalsegmentator',
         'ts_id': 292
@@ -61,8 +74,71 @@ definition_sources = {
         'source': 'huggingface',
         'hf_id': 'louisblankemeier/stanford_spine',
         'local_id': 'stanford-spine',
+    },
+    'Stanford-Tissue': {
+        'source': 'huggingface',
+        'hf_id': 'stanfordmimi/multilevel_muscle_adipose_tissue',
+        'local_id': 'stanford-tissue',
+    },
+    'BodyAndOrganAnalysis': {
+        'source': 'github',
+        'url': 'https://github.com/UMEssen/Body-and-Organ-Analysis/releases/download/BCA-BodyRegionsWeights-v0.1.0/Task542_BCA_inference.zip',
+        'local_id': 'boa'
     }
 }
+
+# Helper function to download files with retries and progress bar
+def download_file(url, dest_path, retries=5, delay=5):
+    for attempt in range(retries):
+        try:
+            headers = {}
+            if dest_path.exists():
+                headers['Range'] = f'bytes={dest_path.stat().st_size}-'
+            response = requests.get(url, stream=True, headers=headers, timeout=10)
+            response.raise_for_status()
+            total_size = int(response.headers.get('content-length', 0)) + dest_path.stat().st_size if 'Range' in headers else int(response.headers.get('content-length', 0))
+            mode = 'ab' if 'Range' in headers else 'wb'
+            with open(dest_path, mode) as file, tqdm(
+                desc=dest_path.name,
+                total=total_size,
+                initial=dest_path.stat().st_size,
+                unit='B',
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as bar:
+                for data in response.iter_content(chunk_size=1024):
+                    size = file.write(data)
+                    bar.update(size)
+            # Check if the download is complete
+            if dest_path.stat().st_size == total_size:
+                return
+            else:
+                logging.warning(f"Download incomplete: {dest_path.stat().st_size} of {total_size} bytes downloaded.")
+        except requests.RequestException as e:
+            logging.error(f"Attempt {attempt + 1} failed: {e}")
+            if attempt + 1 < retries:
+                logging.info(f"Retrying in {delay} seconds...")
+                time.sleep(delay)
+            else:
+                raise
+    logging.error(f"Download not completed after {retries} retries. Try to restart.")
+
+# Helper function to unzip files
+def unzip_file(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+# Helper function to clean up directory structure
+def reduce_directory_dimensions(directory: Path):
+    while True:
+        items = list(directory.iterdir())
+        if len(items) != 1 or not items[0].is_dir():
+            break
+        subdir = items[0]
+        for item in subdir.iterdir():
+            shutil.move(str(item), str(directory))
+        subdir.rmdir()
+
 
 
 def main():
@@ -75,20 +151,19 @@ def main():
     parser = argparse.ArgumentParser(description='Download models from specified sources, place them in the directories specified in the config file')
     parser.add_argument('--pipeline', '-p', type=str,
                         help='Collection of models needed for specific pipeline to be downloaded',
-                        choices=['BodyComposition', 'BodyCompositionFast', 'SarcopeniaTotalSegmentator', 'SarcopeniaTotalSegmentatorFast', 'SarcopeniaStanford'])
+                        choices=['BodyComposition', 'BodyCompositionFast',
+                                 'SarcopeniaTotalSegmentator', 'SarcopeniaTotalSegmentatorFast',
+                                 'SarcopeniaStanfordFast',
+                                 'BodyAndOrganAnalysis'])
     parser.add_argument('--model', '-m', type=str,
                         help='Single model to be downloaded',
                         choices=['VertebralBodiesCT-ResEncM', 'VertebralBodiesCT-ResEncL', 'BodyCompositionCT-ResEncM', 'BodyCompositionCT-ResEncL',
-                                 'TotalSegmentator-total','TotalSegmentator-body', 'TotalSegmentator-vertebrae_body', 'TotalSegmentator-tissue_types',
-                                 'Stanford-Spine'])
+                                 'TotalSegmentator-total', 'TotalSegmentator-spine', 'TotalSegmentator-body', 'TotalSegmentator-vertebrae_body', 'TotalSegmentator-tissue_types',
+                                 'Stanford-Spine', 'Stanford-Tissue',
+                                 'BodyAndOrganAnalysis'])
     args = parser.parse_args()
 
     # set up logging
-    path_log = Path(f'./logs/pre_downloadmodels_{int(time())}.log')
-    path_log.parent.mkdir(parents=True, exist_ok=True)
-    logging.basicConfig(level=logging.INFO, 
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename=path_log)
     logging.info('LOAD MODELS')
     
     # load config
@@ -96,11 +171,15 @@ def main():
     config_weights = config_dict['paths']['weights']
     logging.info('config loaded')
 
+    # create cache directory
+    cache_dir = Path(config_dict['paths']['cache'])
+    cache_dir.mkdir(parents=True, exist_ok=True)
+
     # load model names
     if args.pipeline:
         model_titles = definition_pipelines[args.pipeline]
     elif args.model:
-        model_titles = [model]
+        model_titles = [args.model]
     else:
         logging.error('No pipeline or model specified')
         return
@@ -127,9 +206,9 @@ def main():
 
     # set up HuggingFace if needed
     if any(definition_sources[model_title]['source'] == 'huggingface' for model_title in model_titles):
-        os.environ["HF_HOME"] = str(config_dict['paths']['huggingface_cache'])
+        os.environ["HF_HOME"] = str(cache_dir)
         from huggingface_hub import snapshot_download 
-        logging.info(f"HuggingFace setup: cache={str(config_dict['paths']['huggingface_cache'])}")
+        logging.info(f"HuggingFace setup: cache={str(cache_dir)}")
 
     # loop through models
     for model_title in model_titles:
@@ -144,6 +223,23 @@ def main():
             logging.info(f"Downloading `TotalSegmentator/{model_title}`, task_id {model['ts_id']} to `{config_weights['totalsegmentator']}`...")
             download_pretrained_weights(model['ts_id'])
             logging.info(f"  finished.")
+        elif model['source'] == 'github':
+            download_dir = config_weights[model['local_id']]
+            logging.info(f"Downloading `{model_title}` from `{model['url']}` to `{download_dir}`...")
+            zip_path = cache_dir / 'download_tmp.zip'
+            try:
+                download_file(model['url'], zip_path)
+                if os.path.exists(download_dir):
+                    logging.info(f"  directory `{download_dir}` already exists, skipping. If something went wrong, delete the directory and try again.")
+                    continue
+                unzip_file(zip_path, download_dir)
+                reduce_directory_dimensions(Path(download_dir))
+                os.remove(zip_path)
+                logging.info(f"  finished.")
+            except Exception as e:
+                logging.error(f"Failed to download or extract `{model_title}`: {e}")
+            logging.info(f"  finished.")
+
         else:
             logging.error(f"Unknown source {model['source']}")
             return
